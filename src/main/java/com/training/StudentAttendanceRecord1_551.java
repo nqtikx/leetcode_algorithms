310126344e490a5be82b367b9c39aa0a82a90e5f
Nikita <miracleqxz@gmail.com>
2026-04-21 13:50:33 +0300

    tasks/StudentAttendanceRecord1_551_will_be_changed

---
+++ src/main/java/com/training/StudentAttendanceRecord1_551.java
@@ -0,0 +1,27 @@
+package com.training;
+
+public class StudentAttendanceRecord1 {
+  public static boolean checkRecord(String s) {
+    if (s.length() <= 1) {
+      return true;
+    }
+    int a = 0;
+    boolean flag = false;
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == 'A') {
+        a++;
+      } else if (s.charAt(i) == 'L') {
+        if (s.length() - 1 != i && i != 0) {
+          if (s.charAt(i - 1) == 'L' && s.charAt(i + 1) == 'L') {
+            flag = true;
+          }
+        }
+      }
+    }
+    return a < 2 && (flag != true);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(checkRecord("LLLL"));
+  }
+}
